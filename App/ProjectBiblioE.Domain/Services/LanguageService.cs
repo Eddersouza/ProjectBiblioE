2b2f3232d638409fb35a91c77d21fe131288824e
EdderSouza <eddye_souza@yahoo.com.br>
2017-01-21 22:50:23 -0200

    Idioma Service

---
+++ App/ProjectBiblioE.Domain/Services/LanguageService.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+using ProjectBiblioE.Domain.Contracts.Filters;
+using ProjectBiblioE.Domain.Contracts.Repository;
+using ProjectBiblioE.Domain.Contracts.Services;
+using ProjectBiblioE.Domain.Entities;
+
+namespace ProjectBiblioE.Domain.Services
+{
+    /// <summary>
+    /// Language service
+    /// </summary>
+    public class LanguageService : LanguageServiceContract
+    {
+        /// <summary>
+        /// Instance of language repository
+        /// </summary>
+        private readonly LanguageRepositoryContract _languageRepository;
+
+        /// <summary>
+        /// Constructor for service language
+        /// </summary>
+        /// <param name="languageRepositoryContract">Instance of repository</param>
+        public LanguageService(LanguageRepositoryContract languageRepositoryContract)
+        {
+            this._languageRepository = languageRepositoryContract;
+        }
+
+        /// <summary>
+        /// Get language for repository
+        /// </summary>
+        /// <param name="filters">Filter to language</param>
+        /// <returns>Language list entity</returns>
+        public List<Language> GetLanguages(LanguageFilter filters)
+        {
+            return this._languageRepository.GetLanguages(filters);
+        }
+    }
+}
