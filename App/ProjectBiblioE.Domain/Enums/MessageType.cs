0f78cb8b2a969b363a8179d18db3bfb0396891f1
EdderSouza <eddye_souza@yahoo.com.br>
2017-02-13 21:28:14 -0200

    Adição de imagens para mensagem Adição dos tipos de mensagem

---
+++ App/ProjectBiblioE.Domain/Enums/MessageType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBiblioE.Domain.Enums
+{
+    /// <summary>
+    /// Represent messages type in the system.
+    /// </summary>
+    public enum MessageType
+    {
+        /// <summary>
+        /// Confirm message.
+        /// </summary>
+        Confirm,
+
+        /// <summary>
+        /// Error message.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// Success message.
+        /// </summary>
+        Success
+    }
+}
